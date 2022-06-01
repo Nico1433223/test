5e11a8a1f863e3c0951c47b62edec00a1b763f76
Nico1433223 <75736278+Nico1433223@users.noreply.github.com>
2022-06-01 13:45:45 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import os
-command = './bomb ans.txt ' #可以直接在命令行中执行的命令
-r = os.popen(command) #执行该命令
+lines=[]
+f=open('./file.txt')
+for line in f.readlines():
+    line.strip()
+    lines.append(line)
+f.close()
 def fun():
     for i in range(0,10):
         for j in range(0,10):
@@ -8,13 +12,14 @@ def fun():
                 for a in range(0,10):
                     for b in range(0,10):
                         for c in range(0,10):
-                            command=str(i)+' '+str(j)+' '+str(k)+' '+str(a)+' '+str(b)+' '+str(c)
+                            lines[5]=str(i)+' '+str(j)+' '+str(k)+' '+str(a)+' '+str(b)+' '+str(c)
+                            s=''.join(lines)
+                            f=open('./file.txt','w+')
+                            f.write(s)
+                            f.close()
+                            command='./bomb file.txt'
                             r = os.popen(command) #执行该命令
-                            info=r.readlines() #读取命令的输出结果
-                            for line in info:  #按行遍历
-                                line = line.strip('\r\n')
-                                if(line=='BOOM!'):
-                                    return [i,j,k,a,b,c]
+                            info=r.read() #读取命令的输出结果
+                            if 'BOOM!' not in info:
+                                return [i,j,k,a,b,c]
 print(fun()) 
-           
-
